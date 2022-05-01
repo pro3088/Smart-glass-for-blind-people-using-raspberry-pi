794e534d27b5f5f9a95e963a0225f1c6643ba8d3
pro3088 <53413092+pro3088@users.noreply.github.com>
2022-05-01 11:54:49 +0100

    Distance

---
+++ triangulation.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 11:55:00 2022
+
+@author: rotim
+"""
+
+import sys
+import cv2
+import numpy as np
+import time
+
+
+def find_depth(right_point, left_point, frame_right, frame_left, baseline,f, alpha):
+
+    # CONVERT FOCAL LENGTH f FROM [mm] TO [pixel]:
+    height_right, width_right, depth_right = frame_right.shape
+    height_left, width_left, depth_left = frame_left.shape
+
+    if width_right == width_left:
+        f_pixel = (width_right * 0.5) / np.tan(alpha * 0.5 * np.pi/180)
+
+    else:
+        print('Left and right camera frames do not have the same pixel width')
+
+    x_right = right_point[0]
+    x_left = left_point[0]
+
+    # CALCULATE THE DISPARITY:
+    disparity = x_left-x_right      #Displacement between left and right frames [pixels]
+
+    # CALCULATE DEPTH z:
+    zDepth = (baseline*f_pixel)/disparity             #Depth in [cm]
+
+    return zDepth
